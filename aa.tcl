5ef9101db122a154ea84f857e020350c94a5506e
Salman Farshi <salman@primesiliconbd.com>
2018-09-20 13:03:02 +0600

    userfriendly reading

--- aa.tcl
+++ aa.tcl
@@ -1,9 +1,10 @@
 #Written By: Md. Salman Farshi
 #Date: September 14, 2018 
+#Updated on: September 20, 2018.
 #File reading script
 set fi [open empl_list.txt r]; #Empl_list reading;
 while {![eof $fi]} {
-   after 1000;
-   set a [gets $fi line]; puts "$a: $line";
+   after 500;
+   set a [gets $fi line]; puts "$line";
    if {[eof $fi] } {puts "File Ended"; break}
 }
