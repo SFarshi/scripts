5a52caf582f69822a49b09a0416788627056729b
Salman Farshi <salman@primesiliconbd.com>
2018-09-20 12:31:58 +0600

    Empl_list reading

--- aa.tcl
+++ aa.tcl
@@ -1,11 +1,9 @@
 #Written By: Md. Salman Farshi
 #Date: September 14, 2018 
 #File reading script
-set fi [open test.txt r]
+set fi [open empl_list.txt r]; #Empl_list reading;
 while {![eof $fi]} {
+   after 1000;
    set a [gets $fi line]; puts "$a: $line";
    if {[eof $fi] } {puts "File Ended"; break}
 }
-
-
-###
