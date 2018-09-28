c2311e2d03192b41ad2e56583504f324ccc200ed
Salman Farshi <salman@primesiliconbd.com>
2018-09-28 12:17:44 +0600

    Empl list and aa.tcl updated.

--- aa.tcl
+++ aa.tcl
@@ -1,10 +1,11 @@
 #Written By: Md. Salman Farshi
 #Date: September 14, 2018 
-#Updated on: September 20, 2018.
 #File reading script
-set fi [open empl_list.txt r]; #Empl_list reading;
+set fi [open test.txt r]
 while {![eof $fi]} {
-   after 500;
-   set a [gets $fi line]; puts "$line";
+   set a [gets $fi line]; puts "$a: $line";
    if {[eof $fi] } {puts "File Ended"; break}
 }
+
+
+###
